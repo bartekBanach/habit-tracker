4eefb1afc3d7e951203eafd69068b5dd8c68a855
bartek <bartek.banach.47@gmail.com>
2024-03-25 20:07:44 +0100

    Initial commit.

---
+++ client/src/types.d.ts
@@ -0,0 +1,14 @@
+interface Item {
+    id: number;
+    title: string;
+    price: number;
+    url: string;
+    image: string;
+    status: string;
+  }
+
+  interface Duration {
+    hours: number;
+    minutes: number;
+    seconds: number;
+  }
