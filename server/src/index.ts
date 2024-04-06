cbfd73e7d029098d1bd7ab98ca373041ad96615f
bartek <bartek.banach.47@gmail.com>
2024-04-07 00:21:02 +0200

    Setup eslint for backend.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,14 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello NOD Readers!');
+app.get("/", (req, res) => {
+  res.send("Hello NOD Readers!");
 });
 
 app.listen(port, () => {
-return console.log(`Express server is listening at http://localhost:${port} 🚀`);
+  return console.log(
+    `Express server is listening at http://localhost:${port} 🚀`,
+  );
 });
