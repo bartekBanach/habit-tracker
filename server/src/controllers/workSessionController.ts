ad86668a11a38b6409ac7d9eb875d6c4cd0629e2
bartek <bartek.banach.47@gmail.com>
2024-07-13 00:33:10 +0200

    Make workSessionController use custom error classes.

--- server/src/controllers/workSessionController.ts
+++ server/src/controllers/workSessionController.ts
@@ -1,73 +1,62 @@
 import { Request, Response } from 'express';
 import WorkSession, { IWorkSession } from '../models/workSession';
-import { startOfWeek, endOfWeek, startOfDay, endOfDay } from 'date-fns';
+import { startOfDay, endOfDay } from 'date-fns';
 import asyncHandler from 'express-async-handler';
+import DatabaseError from '../errors/DatabaseError';
+import BadRequestError from '../errors/BadRequestError';
 
-async function createWorkSession(req: Request, res: Response): Promise<void> {
+const createWorkSession = asyncHandler(async (req: Request, res: Response) => {
+  const { habit, timeDuration } = req.body;
+  if (!habit || !timeDuration) {
+    throw new BadRequestError('Missing required fields');
+  }
   try {
-    const { habit, timeDuration } = req.body;
     const workSession: IWorkSession = new WorkSession({ habit, timeDuration });
     await workSession.save();
     res.status(201).json({ message: 'Work Session created successfully', workSession });
   } catch (error) {
-    if (error instanceof Error) {
-      res.status(500).json({
-        message: 'Failed to create Work Session',
-        error: error.message,
-      });
-    }
+    throw new DatabaseError('Failed to create Work Session');
   }
-}
+});
 
-async function getAllWorkSessions(req: Request, res: Response): Promise<void> {
+const getAllWorkSessions = asyncHandler(async (req: Request, res: Response) => {
   try {
-    const activities: IWorkSession[] = await WorkSession.find();
-    res.json(activities);
+    const workSessions: IWorkSession[] = await WorkSession.find();
+    res.json(workSessions);
   } catch (error) {
-    if (error instanceof Error) {
-      res.status(500).json({ message: 'Failed to get activities', error: error.message });
-    }
+    throw new DatabaseError('Failed to get work sessions');
   }
-}
+});
 
-async function getWorkSessionsByTimePeriod(req: Request, res: Response): Promise<void> {
+const getWorkSessionsByTimePeriod = asyncHandler(async (req: Request, res: Response) => {
+  const { from, to, habitId } = req.query;
+
+  if (!from || !to) {
+    throw new BadRequestError('Request missing required parameteres.');
+  }
   try {
-    const { from, to, habitId } = req.query;
-
-    if (!from || !to) {
-      res.status(400).json({ message: 'habitId, from and parameters are required' });
-      return;
-    }
-
     const query: any = {
       finishedAt: {
         $gte: startOfDay(new Date(from as string)),
         $lt: endOfDay(new Date(to as string)),
       },
     };
-
     if (habitId) {
       query.habit = habitId;
     }
-
     const workSessions: IWorkSession[] = await WorkSession.find(query);
-
     res.json(workSessions);
   } catch (error) {
-    if (error instanceof Error) {
-      res.status(500).json({ message: 'Failed to get work sessions', error: error.message });
-    }
+    throw new DatabaseError('Failed to get work sessions');
   }
-}
+});
 
 const deleteAllWorkSessions = asyncHandler(async (req: Request, res: Response) => {
   try {
     await WorkSession.deleteMany({});
     res.json({ message: 'All sessions deleted successfully' });
   } catch (error) {
-    if (error instanceof Error) {
-      res.status(500).json({ message: 'Failed to delete sessions', error: error.message });
-    }
+    throw new DatabaseError('Failed to delete sessions');
   }
 });
 
